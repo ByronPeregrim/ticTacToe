8a557b0e910b81e25926a83ac4487158b8704bf5
ByronPeregrim <byronperegrim@gmail.com>
2022-01-12 20:23:24 -0500

    created game board and GUI layout

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ticTacToe
 Tic Tac Toe game played in browser.
+
+background image taken from https://wallpapercave.com/w/wp2742875
+
---
+++ script.js
@@ -0,0 +1,33 @@
+// BOARD SETUP
+// create gameboard module
+    // create container for for square objects to be stored in html
+    // store board as array inside of gameboard object
+    // each section, 9 total, will be square object
+        // create factory to create 9 square objects
+            // input squares into html as <li> in an <ul>
+            // use CSS to design and position each square
+
+
+
+
+
+
+
+// use factory to create player objects
+
+// create display controller module/object. controls flow of the game
+
+// Create function that will render the content of the gameboard array to the webpage
+
+// Build the functions that allow players to add marks to a specific spot on the board,
+// and then tie it to the DOM, letting players click on the gameboard to place their marker. 
+
+// three modules. Game. Player. Gameboard.
+
+// build logic to check for winner or tie.
+
+// Add interface for players to input names
+
+// add start/restart button
+
+// add display element that congratulates winner
