e1896c18ecab619be83f0be1b13149568a6b280f
ByronPeregrim <byronperegrim@gmail.com>
2022-01-13 15:25:25 -0500

    changed board to an array, changed boardModule accordingly. Removed square divs from html. Generated new divs from board array.

--- script.js
+++ script.js
@@ -1,52 +1,104 @@
-const Player = (name) => {
-    return {name}
+
+const Player = () => {
+    player1 = {
+        symbol: 'X'
+    };
+    player2 = {
+        symbol: 'O'
+    };
+    const init = () => {
+        cacheDom();
+        attachEventListeners();
+    };
+    const cacheDom = () => {
+        $firstPlayerName = document.querySelector('#firstPlayerName');
+        $secondPlayerName = document.querySelector('#secondPlayerName');
+        $button = document.querySelector('#editNamesButton');
+    }   
+    const attachEventListeners = () => {
+        $button.addEventListener('click', editNames);
+    };
+    const editNames = () => {
+        player1Name = prompt("Enter name for Player X: ", 'Bill');
+        player1.name = player1Name;
+        player2Name = prompt('Enter name for Player O: ', 'Ted');
+        player2.name = player2Name;
+        $firstPlayerName.innerHTML = "Player X: " + player1.name;
+        $secondPlayerName.innerHTML = 'Player O: ' + player2.name;
+    };
+    init();
+    return {player1, player2};
 };
 
-const playerModule = {
-    people: [],
+
+/* const boardModule = {
+    board: [],
     init: function() {
         this.cacheDom();
+        this.createBoard();
         this.attachEventListeners();
     },
     cacheDom: function() {
-        this.$firstPlayerName = document.querySelector('#firstPlayerName');
-        this.$secondPlayerName = document.querySelector('#secondPlayerName');
-        this.$button = document.querySelector('#editNamesButton');
+        this.$board = document.querySelector('.board');
     },
-    attachEventListeners: function () {
-        this.$button.addEventListener('click', playerModule.editNames);
-    },
-    editNames: function () {
-        player1 = prompt("Enter name for Player X: ", 'Bill');
-            player2 = prompt('Enter name for Player O: ', 'Ted');
-            playerModule.$firstPlayerName.innerHTML = "Player X: " + player1;
-            playerModule.$secondPlayerName.innerHTML = 'Player O: ' + player2;
-    }
-};
-
-
-const board = {
-    init: function() {
-        this.cacheDom();
-        this.attachEventListeners();
-    },
-    cacheDom: function() {
-        this.$allSquares = document.querySelectorAll('.square');
+    createBoard: function() {
+        const createSquare = () => {
+            square = document.createElement('div');
+            square.classList.add('square');
+            return square;
+        }
+        for (let i=0; i<9; i++) {
+           boardModule.$board.appendChild(createSquare());
+        }
+        this.$allSquares = this.$board.querySelectorAll('.square');
     },
     attachEventListeners: function() {
         this.$allSquares.forEach(square => {
             square.addEventListener('click', function(e) {
-                board.addMark(e.target, 'X');
+                boardModule.addMark(e.target, 'X');
             });
         });
     },
     addMark : function(ele, symbol) {
         ele.innerHTML = symbol;
-    }
+    },
+};
+*/
+
+const boardModule = {
+    init: function() {
+        this.cacheDom();
+        this.createBoard();
+    },
+    cacheDom: function() {
+        this.$board = document.querySelector('.board');
+    },
+    createBoard: function() {
+        boardArray = [];
+        const pushSquaresToArray = () => {
+            for (let i=0; i<9; i++) {
+                let Square = {symbol: '', number: i+1};
+                boardArray.push(Square);
+            };
+        };
+        const renderSquares = () => {
+            this.$board.innerHTML = '';
+            for (let item in boardArray) {
+                $square = document.createElement('div');
+                $square.classList.add('square')
+                $square.innerHTML = boardArray[item].number;
+                boardModule.$board.appendChild($square);
+            };
+        };
+        pushSquaresToArray();
+        renderSquares();
+        console.log(boardArray);
+    },
 };
 
 
-const results = {
+
+const resultsModule = {
     init: function() {
         this.cacheDom();
         this.attachEventListeners();
@@ -56,19 +108,19 @@ const results = {
         this.$button = document.querySelector('#startNewGameButton');
     },
     attachEventListeners: function() {
-        this.$button.addEventListener('click', results.startNewGame);
+        this.$button.addEventListener('click', resultsModule.startNewGame);
     },
     startNewGame: function() {
-        board.$allSquares.forEach(square => {
+        boardModule.$allSquares.forEach(square => {
             square.innerHTML = '';
         });
     }
 };
 
 
-playerModule.init();
-results.init();
-board.init();
+Player();
+resultsModule.init();
+boardModule.init();
 
 // use factory to create player objects
 
